75302f214e32b6c1e20b2fcc97e5bc297b654384
Gerson Oviedo <gersonoviedo@artistshot.com>
2021-09-16 08:21:25 -0500

    clean project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  return (
+    <div className="container">
+      <h2>react test</h2>
+    </div>
+  );
+}
+
+export default App;
