d4d812299f82f86e8045a949f8dae42a8740ea54
edwardramirez31 <66492063+edwardramirez31@users.noreply.github.com>
2021-09-30 12:01:13 -0500

    React App Template
    This is a react app template that I created in order to start the technical interview as soon as possible.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Container, Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxWidth="md">
+        <Typography variant="h3">Hello World!</Typography>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
